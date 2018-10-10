a4559e2d9dc35ce0dd567eccca4fa4a6bf17c7f3
JoyVrooland <bjh.vrooland@student.alfa-college.nl>
2018-10-10 11:56:49 +0200

    Alles vertaald naar het Nederlands

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -6,20 +6,21 @@ use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CustomerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('name_prefix')
-            ->add('street')
-            ->add('house_number')
-            ->add('post_code')
-            ->add('city')
-            ->add('phone')
+            ->add('firstname',TextType::class, array('label' => 'Voornaam') )
+            ->add('lastname',TextType::class, array('label' => 'Achternaam') )
+            ->add('name_prefix',TextType::class, array('label' => 'Tussenvoegsel') )
+            ->add('street',TextType::class, array('label' => 'Straatnaam') )
+            ->add('house_number',TextType::class, array('label' => 'Huisnummer') )
+            ->add('post_code',TextType::class, array('label' => 'Post code') )
+            ->add('city',TextType::class, array('label' => 'Woonplaats') )
+            ->add('phone',TextType::class, array('label' => 'Telefoonnummer') )
             ->add('email')
         ;
     }
--- src/Form/MaintenanceType.php
+++ src/Form/MaintenanceType.php
@@ -6,16 +6,17 @@ use App\Entity\Maintenance;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MaintenanceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('discription')
-            ->add('duration')
-            ->add('price')
+            ->add('title',TextType::class, array('label' => 'Titel') )
+            ->add('discription',TextType::class, array('label' => 'Beschrijving') )
+            ->add('duration',TextType::class, array('label' => 'Tijds duur') )
+            ->add('price',TextType::class, array('label' => 'Prijs') )
         ;
     }
 
