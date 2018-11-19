5f045e52a4579568dbfb5f6ecaa6cb727fcd248a
JoyVrooland <bjh.vrooland@student.alfa-college.nl>
2018-11-19 10:10:13 +0100

    jajoh

---
+++ src/Entity/Availability.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AvailabilityRepository")
+ */
+class Availability
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $branch;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $workDate;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $hours;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBranch(): ?string
+    {
+        return $this->branch;
+    }
+
+    public function setBranch(string $branch): self
+    {
+        $this->branch = $branch;
+
+        return $this;
+    }
+
+    public function getWorkDate(): ?\DateTimeInterface
+    {
+        return $this->workDate;
+    }
+
+    public function setWorkDate(\DateTimeInterface $workDate): self
+    {
+        $this->workDate = $workDate;
+
+        return $this;
+    }
+
+    public function getHours(): ?float
+    {
+        return $this->hours;
+    }
+
+    public function setHours(float $hours): self
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+}
